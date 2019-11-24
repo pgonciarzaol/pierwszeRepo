36d1591ab05d8e7c6706fdf2b24f38b5bd66c652
FraQu <xa.kasprzyk@gmail.com>
2019-11-24 09:26:14 +0100

    Adding number works now

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,12 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace repo1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("1:. Dodaj Liczby");
+                Console.WriteLine("2:. Odejmij Liczby");
+                Console.WriteLine("3:. * Liczby");
+                Console.WriteLine("4:. Dziel Liczby");
+                Console.WriteLine("5:. Wyjscie");
+                int menuOption = Convert.ToInt32(Console.ReadLine());
+                if (menuOption == 5)
+                    break;
+
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                if(menuOption == 1)
+                {
+                    Console.WriteLine(a + b);
+                }
+            }
+
         }
     }
 }
